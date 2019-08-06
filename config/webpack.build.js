3539d1778a0d572d2c925603a46aeba583bc59da
Pakhoalot <pakholeung37@gmail.com>
2019-08-06 22:48:20 +0800

    change some config

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, '../src/main.js'),
   output: {
@@ -32,7 +32,11 @@ module.exports = {
   },
   plugins: [
     // 请确保引入这个插件！
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new copyWebpackPlugin([{
+      from: path.resolve(__dirname, '../public'),    //要打包的静态资源目录地址，这里的__dirname是指项目目录下，是node的一种语法，可以直接定位到本机的项目目录中
+      to: './'  //要打包到的文件夹路径，跟随output配置中的目录。所以不需要再自己加__dirname
+  }])
   ],
   
   mode: 'production'
